04836e0c18a12cf74cc8f5fd634083968553420a
JackG <jackgiannaris@gmail.com>
2018-09-18 08:12:46 -0400

    Variables

--- src/BMI.java
+++ src/BMI.java
@@ -4,11 +4,28 @@ public class BMI {
 
     public static void main(String[] args){
 
+        //declare
+        int feet;
+        int inches;
+        int weight;
+        int height;
+        String name;
+        double BMI;
+        double InchesToMeters;
+        double PoundsToKilometers;
+        
+
+
         Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        System.out.println("what is your name");
+        name = keyboard.nextLine();
 
+        System.out.println("What is your weight");
+        weight = keyboard.nextLine();
 
-
-
+        System.out.println("What is your height(in inches)");
+        height = keyboard.nextLine();
 
 
 
