978faf51a30f43c447da94827f6655a1bdaa0b1e
JackG <jackgiannaris@gmail.com>
2018-09-20 08:10:24 -0400

    Finished BMI project

--- src/BMI.java
+++ src/BMI.java
@@ -5,27 +5,35 @@ public class BMI {
     public static void main(String[] args){
 
         //declare
-        int feet;
-        int inches;
-        int weight;
-        int height;
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
         String name;
+
+        int weight, height;
         double BMI;
         double InchesToMeters;
-        double PoundsToKilometers;
+        double PoundsToKilograms;
+
         
 
 
-        Scanner keyboard;
-        keyboard = new Scanner(System.in);
         System.out.println("what is your name");
         name = keyboard.nextLine();
 
         System.out.println("What is your weight");
-        weight = keyboard.nextLine();
+        weight = keyboard.nextInt();
 
         System.out.println("What is your height(in inches)");
-        height = keyboard.nextLine();
+        height = keyboard.nextInt();
+        InchesToMeters = height *(0.0254/1);
+        System.out.println("Your height from inches to meters is "+InchesToMeters+"m");
+        PoundsToKilograms =  weight *(0.453592/1);
+        System.out.println("Your weight from pounds to kilometers is "+PoundsToKilograms+"kg");
+        BMI = PoundsToKilograms/ InchesToMeters*InchesToMeters;
+        System.out.println("Your BMI is "+BMI+".");
+
+
+
 
 
 
